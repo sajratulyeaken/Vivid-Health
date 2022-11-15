8834c30ef102d5044d0d7d7e67f05df6edb6ee2d
Sajratul Yeaken <sajratulyeaken@iut-dhaka.edu>
2022-11-15 22:44:07 +0600

    Homescreen Product Listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Container } from 'react-bootstrap';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Homescreen from "./screens/Homescreen";
 
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to Vivid Health!</h1>
+          <Homescreen />
         </Container>
       </main>
       <Footer />
---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Bisocor 2.5',
+    image: '/images/Bisocor.jpg',
+    description:
+      'Bisocor 2.5 is indicated in Hypertension, Angina and moderate to severe heart failure',
+    manufacturer: 'Square Pharmaceuticals Limited',
+    category: 'Medicines',
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Bizoran 5/40',
+    image: '/images/Bizoran.jpg',
+    description:
+      'It is suitable for treating hypertension alone or together with other antihypertensive drugs to lower blood pressure.',
+    manufacturer: 'Beximco Pharmaceuticals Ltd.',
+    category: 'Medicines',
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Clopid 75',
+    image: '/images/Clopid.jpg',
+    description:
+      'It is prescribed to individuals with acute ST-elevation myocardial infarction to minimize the risk of myocardial infarction and stroke (STEMI).',
+    manufacturer: 'Drug International Limited',
+    category: 'Medicines',
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Sergel 20',
+    image: '/images/Sergel.jpg',
+    description:
+      'Sergel 20 is used to treat chronic heartburn and other symptoms associated with GERD.',
+    manufacturer: 'Healthcare Pharmaceuticals Limited',
+    category: 'Medicines',
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Monas 10',
+    image: '/images/Monas.jpg',
+    description:
+      'Monas 10 is prescribed for the prevention and treatment of asthma.',
+    manufacturer: 'The ACME Laboratories Ltd.',
+    category: 'Medicines',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Exforge 5/160',
+    image: '/images/Exforge.jpg',
+    description:
+      'Exforge 5/160 Tablet is used to treat hypertension. This Exforge 5/160 Tablet is not recommended for the treatment of hypertension as a first-line treatment.',
+    manufacturer: 'Novartis(Bangladesh) Limited',
+    category: 'Medicines',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
